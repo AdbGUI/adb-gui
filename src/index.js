f7368fe3272d9794f89c7e9f0e893c5f5d1c6d3c
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2021-11-21 19:23:13 -0400

    Add(Routing): Add routing into splash screen and main

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 import './index.css';
 import SplashScreen from './Screens/SplashScreen';
+import MainScreen from './Screens/MainScreen';
 
 ReactDOM.render(
     <React.StrictMode>
-        <SplashScreen enableTips={true} />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/splashscreen" element={<SplashScreen enableTips={true} />} />
+                <Route exact path="/" element={<MainScreen />} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
