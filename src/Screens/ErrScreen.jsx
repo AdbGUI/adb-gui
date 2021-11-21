c10e547d602b9dadcbc6e65193610a5e093198eb
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2021-11-21 04:50:10 -0400

    feature(Prepare Routing): Prepare to route screens

---
+++ src/Screens/ErrScreen.jsx
@@ -0,0 +1,11 @@
+
+const ErrScreen = () => {
+    return (
+        <div className="err-screen">
+            <h1>404</h1>
+            <h2>Page not found</h2>
+        </div>
+    );
+};
+
+export default ErrScreen;
---
+++ src/Screens/HomeScreen.jsx
@@ -0,0 +1,10 @@
+
+const HomeScreen = () => {
+    return (
+        <main>
+            <h1>Home</h1>
+        </main>
+    );
+};
+
+export default HomeScreen;
