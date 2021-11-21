30eb42219b262a725a1f7f7ea017c5168927852a
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2021-11-21 04:45:54 -0400

    feature(Add main font): Add Montserrat like as a Main Font

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SplashScreen from './Screens/SplashScreen';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <SplashScreen enableTips={true} />
     </React.StrictMode>,
     document.getElementById('root')
 );
