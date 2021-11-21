fc1454556ace3be07f9605f19c0be31781deb93e
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2021-11-21 19:27:41 -0400

    Refactor(Correct format and add feature): Correct formating and separate splashscreen, add command to close the splashscreen

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,21 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-    )]
+use tauri::Manager;
+
+// Command to close the splash screen
+#[tauri::command]
+fn close_splashscreen(window: tauri::Window) {
+    // Close splashscreen
+    if let Some(splashscreen) = window.get_window("splashscreen") {
+        splashscreen.close().unwrap();
+    }
+    // Show main window
+    window.get_window("main").unwrap().show().unwrap();
+}
 
 fn main() {
+    let context = tauri::generate_context!();
+
     tauri::Builder::default()
-        .run(tauri::generate_context!())
+        .invoke_handler(tauri::generate_handler![close_splashscreen])
+        .run(context)
         .expect("error while running tauri application");
 }
