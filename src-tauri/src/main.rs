e2f0585c0d6cc4de0ef8247e03bc5abf954581ad
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2021-11-29 02:44:14 -0400

    feat: make close splashscreen a command to controlle this from frontend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,32 +1,39 @@
-mod install;
-use install::*;
+use std::sync::{Arc, Mutex};
+use tauri::{generate_context, Manager, State, Window};
 
-use tauri::Manager;
+mod utils;
+mod install;
+mod config;
+use install::Installer;
+
+// wrappers around each Window
+// we use a dedicated type because Tauri can only manage a single instance of a given type
+struct SplashscreenWindow(Arc<Mutex<Window>>);
+struct MainWindow(Arc<Mutex<Window>>);
+
+#[allow(dead_code)]
+#[tauri::command]
+fn close_splashscreen( _: Window, splashscreen: State<SplashscreenWindow>, main: State<MainWindow>,) {
+    // Close splashscreen
+    splashscreen.0.lock().unwrap().close().unwrap();
+    // Show main window
+    main.0.lock().unwrap().show().unwrap();
+}
 
 fn main() {
-    let context = tauri::generate_context!();
+    let context = generate_context!();
+    let _app_settings = &context.config().package.version;
 
     tauri::Builder::default()
         .setup(|app| {
-            let splashscreen_window = app.get_window("splashscreen").unwrap();
-            let main_window = app.get_window("main").unwrap();
-            let install_window = app.get_window("install").unwrap();
-            // we perform the initialization code on a new task so the app doesn't freeze
-            tauri::async_runtime::spawn(async move {
-                // initialize your app here instead of sleeping :)
-                println!("Initializing...");
-                std::thread::sleep(std::time::Duration::from_secs(2));
-                println!("Done initializing.");
+            // set the splashscreen and main windows to be globally available with the tauri state API
+            app.manage(SplashscreenWindow(Arc::new(Mutex::new(
+                app.get_window("splashscreen").unwrap(),
+            ))));
+            app.manage(MainWindow(Arc::new(Mutex::new(
+                app.get_window("main").unwrap(),
+            ))));
 
-                // After it's done, close the splashscreen and display the main window
-                splashscreen_window.close().unwrap();
-                if verify_installed_tools() {
-                    main_window.show().unwrap();
-                } else {
-                    install_window.show().unwrap();
-                    install_window.emit("install-path", get_install_path().unwrap().to_str()).unwrap();
-                }
-            });
             Ok(())
         })
         .run(context)
